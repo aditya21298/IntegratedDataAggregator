f23b2c48fa7320814794b775006b5fac4f9e654c
aditya <aditya21298@gmail.com>
2024-12-10 23:32:41 +0530

    added h2 properties and mock controller

---
+++ src/main/java/com/example/ida/IntegratedDataAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedDataAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratedDataAggregatorApplication.class, args);
+	}
+
+}
